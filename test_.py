d9282a9e8e021ac4cb0a07a2cacee45c73fc0468
pthomson8 <petercahillthomson@gmail.com>
2020-04-07 23:31:49 +0100

    3rd test to check key mismatch - this currently fails as there is rubbish error handling

--- test_.py
+++ test_.py
@@ -5,8 +5,11 @@ from load_data import load_data
 
 import pandas as pd
 import pytest
+import io
+import sys
 
 #Similarity module tests:
+#check that the similarity score makes sense in a basic test
 def test_sim():
     #expected score 1
     word = "dogs"
@@ -15,22 +18,30 @@ def test_sim():
 
     assert int(df['sim_score_' + word].values) == 1
 
-#load_data tests
+#load_data tests:
+#Happy path using test test_download
 def test_load_data(monkeypatch):
 
-    # monkeypatch the "input" function, so that it returns "Mark".
-    # This simulates the user entering "Mark" in the terminal:
+    # monkeypatch the "input" function, so that it returns "test_download".
+    # This simulates the user entering "test_download" in the terminal:
     #test_download will both act as a suitable key for data and an escape for the while loop
     monkeypatch.setattr('builtins.input', lambda x: "test_download")
 
-    # go about using input() like you normally would:
     i = load_data()
     df = i["test_download"].dataframe
 
     assert isinstance(df, pd.DataFrame)
 
-
-
-#Happy path using test test_download
 #Keys don't match
+def test_load_data_fail1(monkeypatch):
+
+    monkeypatch.setattr('builtins.input', lambda x: "an unlikely anme for a dataset")
+    capturedOutput = io.StringIO()          # Create StringIO object
+    sys.stdout = capturedOutput                   #  and redirect stdout.
+
+    i = load_data()
+    df = i["an unlikely anme for a dataset"].dataframe
+    sys.stdout = sys.__stdout__                   # Reset redirect.
+
+    assert ("error" in capturedOutput.getvalue() or "Error" in capturedOutput.getvalue() or "ERROR" in capturedOutput.getvalue() ) and not isinstance(df, pd.DataFrame)
 #Not a csv
